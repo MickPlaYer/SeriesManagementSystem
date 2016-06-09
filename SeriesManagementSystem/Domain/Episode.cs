ae8fdc511297c0b7b7079c2e8872505e1ffbef0c
bobshih <bobshih@hotmail.com>
2016-06-09 13:40:44 +0800

    add episode class

---
+++ SeriesManagementSystem/Domain/Episode.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeriesManagementSystem.Domain
+{
+    public class Episode
+    {
+        private string _name;
+        private string _description;
+        private int _episodeID;
+
+        public Episode(string name, string description, int id)
+        {
+            _name = name;
+            _description = description;
+            _episodeID = id;
+        }
+
+        #region Public Properties
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+            set
+            {
+                _description = value;
+            }
+        }
+
+        public int EpisodeID
+        {
+            get
+            {
+                return _episodeID;
+            }
+        }
+        #endregion
+    }
+}
---
+++ SeriesManagementSystemUnitTest/EpisodeUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeriesManagementSystem.Domain;
+
+namespace SeriesManagementSystemUnitTest
+{
+    [TestClass]
+    public class EpisodeUnitTest
+    {
+        Episode _episode;
+        const int ID = 3;
+        const string EPISODE_NAME = "episode name";
+        const string EPISODE_DESCRIPTION = "epispode description";
+        const string MODIFIED_NAME = "modified name";
+        const string MODIFIED_DESCRIPTION = "modified description";
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _episode = new Episode(EPISODE_NAME, EPISODE_DESCRIPTION, ID);
+        }
+
+        [TestMethod]
+        public void TestProperties()
+        {
+            Assert.AreEqual(ID, _episode.EpisodeID);
+            Assert.AreEqual(EPISODE_NAME, _episode.Name);
+            Assert.AreEqual(EPISODE_DESCRIPTION, _episode.Description);
+
+            _episode.Name = MODIFIED_NAME;
+            _episode.Description = MODIFIED_DESCRIPTION;
+            Assert.AreEqual(MODIFIED_NAME, _episode.Name);
+            Assert.AreEqual(MODIFIED_DESCRIPTION, _episode.Description);
+        }
+    }
+}
