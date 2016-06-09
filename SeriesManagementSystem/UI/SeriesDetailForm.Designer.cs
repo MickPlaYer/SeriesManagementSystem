d4bec10425a3e0027c74479cc4dfb14de72eb504
bobshih <bobshih@hotmail.com>
2016-06-09 23:53:39 +0800

    series detail form part one

---
+++ SeriesManagementSystem/UI/SeriesDetailForm.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace SeriesManagementSystem.UI
+{
+    partial class SeriesDetailForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // SeriesDetailForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 295);
+            this.Name = "SeriesDetailForm";
+            this.Text = "SeriesDetailForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
